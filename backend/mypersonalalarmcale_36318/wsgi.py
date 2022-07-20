09f78e15f4aa8317176d6ab5a09caa745bcbc7a7
Crowdbotics <team@crowdbotics.com>
2022-07-20 21:01:28 +0000

    Update models

--- backend/mypersonalalarmcale_36318/wsgi.py
+++ backend/mypersonalalarmcale_36318/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mypersonalalarmcale_36318.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mypersonalalarmcale_36318.settings")
 
 application = get_wsgi_application()
